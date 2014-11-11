Juniper/contrail-controller

Test code for Database Purge Closes-Bug: #1350265
Change-Id: I5a35e369fe31a810137d2c98cf3a239861a48792

src/opserver/test/analytics_db_test.py
@@ -1,0 +1,101 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+#
+# analytics_db_test.py
+#
+# System tests for analytics
+#
+
+import sys
+builddir = sys.path[0] + '/../..'
+import signal
+import gevent
+from gevent import monkey
+monkey.patch_all()
+import os
+import unittest
+import testtools
+import fixtures
+import socket
+from utils.analytics_fixture import AnalyticsFixture
+from mockcassandra import mockcassandra
+import logging
+from pysandesh.util import UTCTimestampUsec
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+
+class AnalyticsDbTest(testtools.TestCase, fixtures.TestWithFixtures):
+
+    @classmethod
+    def setUpClass(cls):
+        if AnalyticsDbTest._check_skip_test() is True:
+            return
+
+        if (os.getenv('LD_LIBRARY_PATH', '').find('build/lib') < 0):
+            if (os.getenv('DYLD_LIBRARY_PATH', '').find('build/lib') < 0):
+                assert(False)
+
+        cls.cassandra_port = AnalyticsDbTest.get_free_port()
+        mockcassandra.start_cassandra(cls.cassandra_port)
+
+    @classmethod
+    def tearDownClass(cls):
+        if AnalyticsDbTest._check_skip_test() is True:
+            return
+
+        mockcassandra.stop_cassandra(cls.cassandra_port)
+        pass
+
+    def test_00_database_purge_query(self):
+        '''
+        This test starts redis,vizd,opserver and qed
+        It uses the test class' cassandra instance
+        and checks if database purge functonality is
+        is working properly
+        '''
+        logging.info("*** test_00_database_purge_query ***")
+        if AnalyticsDbTest._check_skip_test() is True:
+            return True
+
+        vizd_obj = self.useFixture(
+            AnalyticsFixture(logging,
+                             builddir,
+                             self.__class__.cassandra_port))
+        assert vizd_obj.verify_on_setup()
+        assert vizd_obj.verify_collector_obj_count()
+        end_time = UTCTimestampUsec()
+        start_time = end_time - 3600*pow(10,6)
+        assert vizd_obj.verify_database_purge(start_time, end_time)
+        assert vizd_obj.verify_purge_query(start_time, end_time)
+        return True
+    # end test_00_database_purge_query
+
+    @staticmethod
+    def get_free_port():
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cs.bind(("", 0))
+        cport = cs.getsockname()[1]
+        cs.close()
+        return cport
+
+    @staticmethod
+    def _check_skip_test():
+        if (socket.gethostname() == 'build01'):
+            logging.info("Skipping test")
+            return True
+        return False
+
+# end class AnalyticsDbTest
+
+def _term_handler(*_):
+    raise IntSignal()
+
+if __name__ == '__main__':
+    gevent.signal(signal.SIGINT,_term_handler)
+    unittest.main(catchbreak=True)

src/opserver/test/utils/analytics_fixture.py
@@ -167,6 +167,8 @@ class OpServer(object):
                 '--redis_server_port', str(self._redis_port),
                 '--redis_query_port', 
                 str(self.analytics_fixture.redis_uves[0].port),
+                '--cassandra_server_list', '127.0.0.1:' +
+                str(self.analytics_fixture.cassandra_port),
                 '--http_server_port', str(self.http_port),
                 '--log_file', self._log_file,
                 '--rest_api_port', str(self.listen_port)]
@@ -1567,6 +1569,43 @@ class AnalyticsFixture(fixtures.Fixture):
         assert(len(res)>0)
         return True
     # end verify_where_query
+
+    @retry(delay=1, tries=5)
+    def verify_database_purge(self, start_time, end_time):
+        self.logger.info('Verify database purge');
+        vns = VerificationOpsSrv('127.0.0.1', self.opserver_port);
+        query = Query(table='ObjectCollectorInfo',
+                             start_time=start_time, end_time=end_time,
+                             select_fields=['ObjectLog'])
+        json_qstr = json.dumps(query.__dict__)
+        res = vns.post_query_json(json_qstr)
+        self.logger.info("log before purging: %s" % res)
+        if not res:
+            return False
+        else:
+            assert(len(res) > 0)
+        json_qstr = json.dumps({'purge_input': 100})
+        res = vns.post_purge_query_json(json_qstr)
+        assert(res == 'started')
+        return True
+    # end verify_database_purge
+
+    @retry(delay=60, tries=5)
+    def verify_purge_query(self, start_time, end_time):
+        self.logger.info('verify_purge_query')
+        vns = VerificationOpsSrv('127.0.0.1', self.opserver_port)
+        query = Query(table='ObjectCollectorInfo',
+                             start_time=start_time, end_time=end_time,
+                             select_fields=['ObjectLog'])
+        json_qstr = json.dumps(query.__dict__)
+        res = vns.post_query_json(json_qstr)
+        self.logger.info("log after purging: %s" % res)
+        if res != []:
+            return False
+        else:
+            assert(len(res) == 0)
+        return True
+    # end verify_purge_query
 
     @retry(delay=1, tries=5)
     def verify_object_table_query(self):

Juniper/contrail-analytics

contrail-opserver/test/analytics_db_test.py
@@ -1,0 +1,103 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+#
+# analytics_db_test.py
+#
+# System tests for analytics
+#
+
+import sys
+builddir = sys.path[0] + '/../..'
+import signal
+import gevent
+from gevent import monkey
+monkey.patch_all()
+import os
+import unittest
+import testtools
+import fixtures
+import socket
+from utils.analytics_fixture import AnalyticsFixture
+from mockcassandra import mockcassandra
+import logging
+from pysandesh.util import UTCTimestampUsec
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+
+class AnalyticsDbTest(testtools.TestCase, fixtures.TestWithFixtures):
+
+    @classmethod
+    def setUpClass(cls):
+        if AnalyticsDbTest._check_skip_test() is True:
+            return
+
+        if (os.getenv('LD_LIBRARY_PATH', '').find('build/lib') < 0):
+            if (os.getenv('DYLD_LIBRARY_PATH', '').find('build/lib') < 0):
+                assert(False)
+
+        cls.cassandra_port = AnalyticsDbTest.get_free_port()
+        mockcassandra.start_cassandra(cls.cassandra_port)
+
+    @classmethod
+    def tearDownClass(cls):
+        if AnalyticsDbTest._check_skip_test() is True:
+            return
+
+        mockcassandra.stop_cassandra(cls.cassandra_port)
+        pass
+
+    def test_00_database_purge_query(self):
+        '''
+        This test starts redis,vizd,opserver and qed
+        It uses the test class' cassandra instance
+        and checks if database purge functonality is
+        is working properly
+        '''
+        logging.info("*** test_00_database_purge_query ***")
+        if AnalyticsDbTest._check_skip_test() is True:
+            return True
+
+        vizd_obj = self.useFixture(
+            AnalyticsFixture(logging,
+                             builddir,
+                             self.__class__.cassandra_port))
+        assert vizd_obj.verify_on_setup()
+        assert vizd_obj.verify_collector_obj_count()
+        end_time = UTCTimestampUsec()
+        start_time = end_time - 3600*pow(10,6)
+        assert vizd_obj.verify_collector_object_log(start_time, end_time)
+        assert vizd_obj.verify_collector_object_log_before_purge(start_time, end_time)
+        assert vizd_obj.verify_database_purge_query(start_time, end_time)
+        assert vizd_obj.verify_collector_object_log_after_purge(start_time, end_time)
+        return True
+    # end test_00_database_purge_query
+
+    @staticmethod
+    def get_free_port():
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cs.bind(("", 0))
+        cport = cs.getsockname()[1]
+        cs.close()
+        return cport
+
+    @staticmethod
+    def _check_skip_test():
+        if (socket.gethostname() == 'build01'):
+            logging.info("Skipping test")
+            return True
+        return False
+
+# end class AnalyticsDbTest
+
+def _term_handler(*_):
+    raise IntSignal()
+
+if __name__ == '__main__':
+    gevent.signal(signal.SIGINT,_term_handler)
+    unittest.main(catchbreak=True)

contrail-opserver/test/utils/analytics_fixture.py
@@ -167,6 +167,8 @@ class OpServer(object):
                 '--redis_server_port', str(self._redis_port),
                 '--redis_query_port', 
                 str(self.analytics_fixture.redis_uves[0].port),
+                '--cassandra_server_list', '127.0.0.1:' +
+                str(self.analytics_fixture.cassandra_port),
                 '--http_server_port', str(self.http_port),
                 '--log_file', self._log_file,
                 '--rest_api_port', str(self.listen_port)]
@@ -1567,6 +1569,49 @@ class AnalyticsFixture(fixtures.Fixture):
         assert(len(res)>0)
         return True
     # end verify_where_query
+
+    def verify_collector_object_log(self, start_time, end_time):
+        self.logger.info('verify_collector_object_log')
+        vns = VerificationOpsSrv('127.0.0.1', self.opserver_port);
+        query = Query(table='ObjectCollectorInfo',
+                             start_time=start_time, end_time=end_time,
+                             select_fields=['ObjectLog'])
+        json_qstr = json.dumps(query.__dict__)
+        res = vns.post_query_json(json_qstr)
+        self.logger.info("collector object log: %s" % res)
+        return res
+    # end verify_collector_object_log
+
+    @retry(delay=1, tries=5)
+    def verify_collector_object_log_before_purge(self, start_time, end_time):
+        self.logger.info('verify_collector_object_log_before_purge')
+        vns = VerificationOpsSrv('127.0.0.1', self.opserver_port);
+        res = self.verify_collector_object_log(start_time, end_time)
+        self.logger.info("collector object log before purging: %s" % res)
+        if not res:
+            return False
+        return True
+        # end verify_collector_object_log_before_purge
+
+    def verify_database_purge_query(self, start_time, end_time):
+        self.logger.info('verify database purge query');
+        vns = VerificationOpsSrv('127.0.0.1', self.opserver_port);
+        json_qstr = json.dumps({'purge_input': 100})
+        res = vns.post_purge_query_json(json_qstr)
+        assert(res == 'started')
+        return True
+    # end verify_database_purge_query
+
+    @retry(delay=2, tries=20)
+    def verify_collector_object_log_after_purge(self, start_time, end_time):
+        self.logger.info('verify_collector_object_log_after_purge')
+        vns = VerificationOpsSrv('127.0.0.1', self.opserver_port)
+        res = self.verify_collector_object_log(start_time, end_time)
+        self.logger.info("collector object log after purging: %s" % res)
+        if res != []:
+            return False
+        return True
+    # end verify_collector_object_log_after_purge
 
     @retry(delay=1, tries=5)
     def verify_object_table_query(self):