Juniper/contrail-controller

These are changes for making cassandra queries/storage more robust: 1. Allow max of 32 outstanding queries in QE 2. Set Leveled Compaction  instead of size-tiered 3. Change Hash collision from log to trace 4. Record query processing errors in QueryPerfInfo
Change-Id: Iaa8fd295c207bd561da946c0549d6f9e895232c8
Partial-Bug: 1469419

src/query_engine/select_fs_query.cc
@@ -148,7 +148,7 @@ void SelectQuery::fs_write_final_result_row(const uint64_t *t,
                         std::make_pair(flow_class_id, *tuple));
             } else {
                 if (!(iter->second == *tuple)) {
-                    QE_LOG(ERROR, "Hash collision for flowclass " << 
+                    QE_TRACE(ERROR, "Hash collision for flowclass " << 
                            iter->second << "and flow class " << *tuple);
                 }
             }

Juniper/contrail-analytics

Conflicts:
	src/query_engine/QEOpServerProxy.cc
Change-Id: I87ebac69efe7f4d3254ee89a29103547b04ddb56

contrail-query-engine/select_fs_query.cc
@@ -148,7 +148,7 @@ void SelectQuery::fs_write_final_result_row(const uint64_t *t,
                         std::make_pair(flow_class_id, *tuple));
             } else {
                 if (!(iter->second == *tuple)) {
-                    QE_LOG(ERROR, "Hash collision for flowclass " << 
+                    QE_TRACE(DEBUG, "Hash collision for flowclass " << 
                            iter->second << "and flow class " << *tuple);
                 }
             }