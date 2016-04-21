Juniper/contrail-controller

This bug adds the CollectorDbStats information to CollectorServerResponse sandesh. Currently the table_stats_map_ (map) to store information about operations per table are diff based i.e., the value stored are minute old. A new datastructure is added to hold a cumulative value. A new function GetCumulativeStats has been introduced in the gendb_statistics.cc to retrieve the values of this map. The existing DbTableStatistics::Get function is modified to iterate the appropriate map to return the values. Closes-Bug: 1558777
Change-Id: I545eb7e356260bf9c0ec1f6c42016f32ccb1a43d

src/analytics/viz_collector.cc
@@ -295,6 +295,13 @@ public:
         if (vsc->Analytics()->GetCqlMetrics(&cmetrics)) {
             resp->set_cql_metrics(cmetrics);
         }
+        // Get cumulative CollectorDbStats
+        std::vector<GenDb::DbTableInfo> vdbti, vstats_dbti;
+        GenDb::DbErrors dbe;
+        collector->GetDbCumulativeStats(&vdbti, &dbe, &vstats_dbti);
+        resp->set_table_info(vdbti);
+        resp->set_errors(dbe);
+        resp->set_statistics_table_info(vstats_dbti);
         // Send the response
         resp->set_context(req->context());
         resp->Response();

Juniper/contrail-analytics

contrail-collector/viz_collector.cc
@@ -295,6 +295,14 @@ public:
         if (vsc->Analytics()->GetCqlMetrics(&cmetrics)) {
             resp->set_cql_metrics(cmetrics);
         }
+        // Get cumulative CollectorDbStats
+        std::vector<GenDb::DbTableInfo> vdbti, vstats_dbti;
+        GenDb::DbErrors dbe;
+        DbHandlerPtr db_handler(vsc->Analytics()->GetDbHandler());
+        db_handler->GetCumulativeStats(&vdbti, &dbe, &vstats_dbti);
+        resp->set_table_info(vdbti);
+        resp->set_errors(dbe);
+        resp->set_statistics_table_info(vstats_dbti);
         // Send the response
         resp->set_context(req->context());
         resp->Response();