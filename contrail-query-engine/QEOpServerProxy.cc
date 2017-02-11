Juniper/contrail-controller

Use contrail_rapidjson as namespace to allow multiple versions
Different versions rapidjson code which is headers only though still needs to
be kept separately as they contain templates. When instantiated, class from
one version can collide with another and produce undesired and unexpected
outcome.

Use a separate namespace contrail_rapidjson as a fix in all of contrail code

Change-Id: I9a246e2ad8552a29f39422f7f66690fa676262ae
Partial-Bug: #1632470

src/query_engine/QEOpServerProxy.cc
@@ -91,24 +91,24 @@ public:
     };
 
     void JsonInsert(std::vector<query_column> &columns,
-            rapidjson::Document& dd,
+            RAPIDJSON_NAMESPACE::Document& dd,
             std::pair<const string,string> * map_it) {
 
         bool found = false;
         for (size_t j = 0; j < columns.size(); j++)
         {
             if ((0 == map_it->first.compare(0,5,string("COUNT")))) {
-                rapidjson::Value val(rapidjson::kNumberType);
+                RAPIDJSON_NAMESPACE::Value val(RAPIDJSON_NAMESPACE::kNumberType);
                 unsigned long num = 0;
                 stringToInteger(map_it->second, num);
                 val.SetUint64(num);
-                rapidjson::Value vk;
+                RAPIDJSON_NAMESPACE::Value vk;
                 dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 found = true;
             } else if (columns[j].name == map_it->first) {
                 if (map_it->second.length() == 0) {
-                    rapidjson::Value val(rapidjson::kNullType);
-                    rapidjson::Value vk;
+                    RAPIDJSON_NAMESPACE::Value val(RAPIDJSON_NAMESPACE::kNullType);
+                    RAPIDJSON_NAMESPACE::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                     found = true;
                     continue;
@@ -118,29 +118,29 @@ public:
                 if (columns[j].datatype == "string" || 
                     columns[j].datatype == "uuid")
                 {
-                    rapidjson::Value val(rapidjson::kStringType);
+                    RAPIDJSON_NAMESPACE::Value val(RAPIDJSON_NAMESPACE::kStringType);
                     val.SetString(map_it->second.c_str(), dd.GetAllocator());
-                    rapidjson::Value vk;
+                    RAPIDJSON_NAMESPACE::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 } else if (columns[j].datatype == "ipaddr") {
-                    rapidjson::Value val(rapidjson::kStringType);
+                    RAPIDJSON_NAMESPACE::Value val(RAPIDJSON_NAMESPACE::kStringType);
                    val.SetString(map_it->second.c_str(),
                                  dd.GetAllocator());
-                    rapidjson::Value vk;
+                    RAPIDJSON_NAMESPACE::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
 
                 } else if (columns[j].datatype == "double") {
-                    rapidjson::Value val(rapidjson::kNumberType);
+                    RAPIDJSON_NAMESPACE::Value val(RAPIDJSON_NAMESPACE::kNumberType);
                     double dval = (double) strtod(map_it->second.c_str(), NULL);
                     val.SetDouble(dval);
-                    rapidjson::Value vk;
+                    RAPIDJSON_NAMESPACE::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 } else {
-                    rapidjson::Value val(rapidjson::kNumberType);
+                    RAPIDJSON_NAMESPACE::Value val(RAPIDJSON_NAMESPACE::kNumberType);
                     unsigned long num = 0;
                     stringToInteger(map_it->second, num);
                     val.SetUint64(num);
-                    rapidjson::Value vk;
+                    RAPIDJSON_NAMESPACE::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 }
                 found = true;
@@ -184,7 +184,7 @@ public:
             QEOpServerProxy::BufferT::iterator res_it;
             for (res_it = raw_result->begin(); res_it != raw_result->end(); ++res_it) {
                 std::map<std::string, std::string>::iterator map_it;
-                rapidjson::Document dd;
+                RAPIDJSON_NAMESPACE::Document dd;
                 dd.SetObject();
 
                 for (map_it = (*res_it).first.begin(); 
@@ -192,8 +192,8 @@ public:
                     // search for column name in the schema
                     JsonInsert(columns, dd, &(*map_it));
                 }
-                rapidjson::StringBuffer sb;
-                rapidjson::Writer<rapidjson::StringBuffer> writer(sb);
+                RAPIDJSON_NAMESPACE::StringBuffer sb;
+                RAPIDJSON_NAMESPACE::Writer<RAPIDJSON_NAMESPACE::StringBuffer> writer(sb);
                 dd.Accept(writer);
                 raw_json->push_back(sb.GetString());
             }

src/query_engine/query.h
@@ -465,13 +465,13 @@ typedef boost::function<void (void *, QEOpServerProxy::QPerfInfo,
 class WhereQuery : public QueryUnit {
 public:
 
-    bool StatTermParse(QueryUnit *main_query, const rapidjson::Value& where_term,
+    bool StatTermParse(QueryUnit *main_query, const RAPIDJSON_NAMESPACE::Value& where_term,
         std::string& pname, match_op& pop,
         GenDb::DbDataValue& pval, GenDb::DbDataValue& pval2,
         std::string& sname, match_op& sop,
         GenDb::DbDataValue& sval, GenDb::DbDataValue& sval2);
 
-    bool StatTermProcess(const rapidjson::Value& where_term,
+    bool StatTermProcess(const RAPIDJSON_NAMESPACE::Value& where_term,
         QueryUnit* pnode, QueryUnit *main_query);
  
     WhereQuery(const std::string& where_json_string, int direction,

src/query_engine/select.cc
@@ -45,18 +45,18 @@ SelectQuery::SelectQuery(QueryUnit *main_query,
     iter = json_api_data.find(QUERY_SELECT);
     QE_PARSE_ERROR(iter != json_api_data.end());
 
-    rapidjson::Document d;
+    RAPIDJSON_NAMESPACE::Document d;
     std::string json_string = "{ \"select\" : " + 
         iter->second + " }";
     json_string_ = json_string;
     QE_TRACE(DEBUG, "parsing through rapidjson: " << json_string);
     d.Parse<0>(const_cast<char *>(json_string.c_str()));
-    const rapidjson::Value& json_select_fields = d["select"]; 
+    const RAPIDJSON_NAMESPACE::Value& json_select_fields = d["select"]; 
     QE_PARSE_ERROR(json_select_fields.IsArray());
     QE_TRACE(DEBUG, "# of select fields is " << json_select_fields.Size());
 
     if (m_query->is_stat_table_query(m_query->table())) {
-        for (rapidjson::SizeType i = 0; i < json_select_fields.Size(); i++) {
+        for (RAPIDJSON_NAMESPACE::SizeType i = 0; i < json_select_fields.Size(); i++) {
             select_column_fields.push_back(json_select_fields[i].GetString());
         }
         stats_.reset(new StatsSelect(m_query, select_column_fields));
@@ -66,7 +66,7 @@ SelectQuery::SelectQuery(QueryUnit *main_query,
     // whether uuid key was provided in select field
     bool uuid_key_selected = false;
 
-    for (rapidjson::SizeType i = 0; i < json_select_fields.Size(); i++) 
+    for (RAPIDJSON_NAMESPACE::SizeType i = 0; i < json_select_fields.Size(); i++) 
     {
         QE_PARSE_ERROR(json_select_fields[i].IsString());
         QE_TRACE(DEBUG, "Select field string: " << 
@@ -429,7 +429,7 @@ query_status_t SelectQuery::process_query() {
             it->get_stattable_info(json_string, u);
 
             //uint64_t thenj = UTCTimestampUsec();
-            rapidjson::Document d;
+            RAPIDJSON_NAMESPACE::Document d;
             if (d.Parse<0>(const_cast<char *>(
                     json_string.c_str())).HasParseError()) {
                 QE_LOG(ERROR, "Error parsing json document: " <<
@@ -440,7 +440,7 @@ query_status_t SelectQuery::process_query() {
 
             std::vector<StatsSelect::StatEntry> attribs;
             {
-                for (rapidjson::Value::ConstMemberIterator itr = d.MemberBegin();
+                for (RAPIDJSON_NAMESPACE::Value::ConstMemberIterator itr = d.MemberBegin();
                         itr != d.MemberEnd(); ++itr) {
                     QE_ASSERT(itr->name.IsString());
 

Juniper/contrail-analytics

contrail-query-engine/QEOpServerProxy.cc
@@ -91,24 +91,24 @@ public:
     };
 
     void JsonInsert(std::vector<query_column> &columns,
-            rapidjson::Document& dd,
+            contrail_rapidjson::Document& dd,
             std::pair<const string,string> * map_it) {
 
         bool found = false;
         for (size_t j = 0; j < columns.size(); j++)
         {
             if ((0 == map_it->first.compare(0,5,string("COUNT")))) {
-                rapidjson::Value val(rapidjson::kNumberType);
+                contrail_rapidjson::Value val(contrail_rapidjson::kNumberType);
                 unsigned long num = 0;
                 stringToInteger(map_it->second, num);
                 val.SetUint64(num);
-                rapidjson::Value vk;
+                contrail_rapidjson::Value vk;
                 dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 found = true;
             } else if (columns[j].name == map_it->first) {
                 if (map_it->second.length() == 0) {
-                    rapidjson::Value val(rapidjson::kNullType);
-                    rapidjson::Value vk;
+                    contrail_rapidjson::Value val(contrail_rapidjson::kNullType);
+                    contrail_rapidjson::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                     found = true;
                     continue;
@@ -118,29 +118,29 @@ public:
                 if (columns[j].datatype == "string" || 
                     columns[j].datatype == "uuid")
                 {
-                    rapidjson::Value val(rapidjson::kStringType);
+                    contrail_rapidjson::Value val(contrail_rapidjson::kStringType);
                     val.SetString(map_it->second.c_str(), dd.GetAllocator());
-                    rapidjson::Value vk;
+                    contrail_rapidjson::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 } else if (columns[j].datatype == "ipaddr") {
-                    rapidjson::Value val(rapidjson::kStringType);
+                    contrail_rapidjson::Value val(contrail_rapidjson::kStringType);
                    val.SetString(map_it->second.c_str(),
                                  dd.GetAllocator());
-                    rapidjson::Value vk;
+                    contrail_rapidjson::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
 
                 } else if (columns[j].datatype == "double") {
-                    rapidjson::Value val(rapidjson::kNumberType);
+                    contrail_rapidjson::Value val(contrail_rapidjson::kNumberType);
                     double dval = (double) strtod(map_it->second.c_str(), NULL);
                     val.SetDouble(dval);
-                    rapidjson::Value vk;
+                    contrail_rapidjson::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 } else {
-                    rapidjson::Value val(rapidjson::kNumberType);
+                    contrail_rapidjson::Value val(contrail_rapidjson::kNumberType);
                     unsigned long num = 0;
                     stringToInteger(map_it->second, num);
                     val.SetUint64(num);
-                    rapidjson::Value vk;
+                    contrail_rapidjson::Value vk;
                     dd.AddMember(vk.SetString(map_it->first.c_str(), dd.GetAllocator()), val, dd.GetAllocator());
                 }
                 found = true;
@@ -184,7 +184,7 @@ public:
             QEOpServerProxy::BufferT::iterator res_it;
             for (res_it = raw_result->begin(); res_it != raw_result->end(); ++res_it) {
                 std::map<std::string, std::string>::iterator map_it;
-                rapidjson::Document dd;
+                contrail_rapidjson::Document dd;
                 dd.SetObject();
 
                 for (map_it = (*res_it).first.begin(); 
@@ -192,8 +192,8 @@ public:
                     // search for column name in the schema
                     JsonInsert(columns, dd, &(*map_it));
                 }
-                rapidjson::StringBuffer sb;
-                rapidjson::Writer<rapidjson::StringBuffer> writer(sb);
+                contrail_rapidjson::StringBuffer sb;
+                contrail_rapidjson::Writer<contrail_rapidjson::StringBuffer> writer(sb);
                 dd.Accept(writer);
                 raw_json->push_back(sb.GetString());
             }

contrail-query-engine/query.h
@@ -465,13 +465,13 @@ typedef boost::function<void (void *, QEOpServerProxy::QPerfInfo,
 class WhereQuery : public QueryUnit {
 public:
 
-    bool StatTermParse(QueryUnit *main_query, const rapidjson::Value& where_term,
+    bool StatTermParse(QueryUnit *main_query, const contrail_rapidjson::Value& where_term,
         std::string& pname, match_op& pop,
         GenDb::DbDataValue& pval, GenDb::DbDataValue& pval2,
         std::string& sname, match_op& sop,
         GenDb::DbDataValue& sval, GenDb::DbDataValue& sval2);
 
-    bool StatTermProcess(const rapidjson::Value& where_term,
+    bool StatTermProcess(const contrail_rapidjson::Value& where_term,
         QueryUnit* pnode, QueryUnit *main_query);
  
     WhereQuery(const std::string& where_json_string, int direction,

contrail-query-engine/select.cc
@@ -45,18 +45,18 @@ SelectQuery::SelectQuery(QueryUnit *main_query,
     iter = json_api_data.find(QUERY_SELECT);
     QE_PARSE_ERROR(iter != json_api_data.end());
 
-    rapidjson::Document d;
+    contrail_rapidjson::Document d;
     std::string json_string = "{ \"select\" : " + 
         iter->second + " }";
     json_string_ = json_string;
     QE_TRACE(DEBUG, "parsing through rapidjson: " << json_string);
     d.Parse<0>(const_cast<char *>(json_string.c_str()));
-    const rapidjson::Value& json_select_fields = d["select"]; 
+    const contrail_rapidjson::Value& json_select_fields = d["select"]; 
     QE_PARSE_ERROR(json_select_fields.IsArray());
     QE_TRACE(DEBUG, "# of select fields is " << json_select_fields.Size());
 
     if (m_query->is_stat_table_query(m_query->table())) {
-        for (rapidjson::SizeType i = 0; i < json_select_fields.Size(); i++) {
+        for (contrail_rapidjson::SizeType i = 0; i < json_select_fields.Size(); i++) {
             select_column_fields.push_back(json_select_fields[i].GetString());
         }
         stats_.reset(new StatsSelect(m_query, select_column_fields));
@@ -66,7 +66,7 @@ SelectQuery::SelectQuery(QueryUnit *main_query,
     // whether uuid key was provided in select field
     bool uuid_key_selected = false;
 
-    for (rapidjson::SizeType i = 0; i < json_select_fields.Size(); i++) 
+    for (contrail_rapidjson::SizeType i = 0; i < json_select_fields.Size(); i++) 
     {
         QE_PARSE_ERROR(json_select_fields[i].IsString());
         QE_TRACE(DEBUG, "Select field string: " << 
@@ -429,7 +429,7 @@ query_status_t SelectQuery::process_query() {
             it->get_stattable_info(json_string, u);
 
             //uint64_t thenj = UTCTimestampUsec();
-            rapidjson::Document d;
+            contrail_rapidjson::Document d;
             if (d.Parse<0>(const_cast<char *>(
                     json_string.c_str())).HasParseError()) {
                 QE_LOG(ERROR, "Error parsing json document: " <<
@@ -440,7 +440,7 @@ query_status_t SelectQuery::process_query() {
 
             std::vector<StatsSelect::StatEntry> attribs;
             {
-                for (rapidjson::Value::ConstMemberIterator itr = d.MemberBegin();
+                for (contrail_rapidjson::Value::ConstMemberIterator itr = d.MemberBegin();
                         itr != d.MemberEnd(); ++itr) {
                     QE_ASSERT(itr->name.IsString());
 