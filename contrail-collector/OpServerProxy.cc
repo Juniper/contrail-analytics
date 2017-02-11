Juniper/contrail-controller

Use contrail_rapidjson as namespace to allow multiple versions
Different versions rapidjson code which is headers only though still needs to
be kept separately as they contain templates. When instantiated, class from
one version can collide with another and produce undesired and unexpected
outcome.

Use a separate namespace contrail_rapidjson as a fix in all of contrail code

Change-Id: I9a246e2ad8552a29f39422f7f66690fa676262ae
Partial-Bug: #1632470

src/analytics/OpServerProxy.cc
@@ -429,7 +429,7 @@ class OpServerProxy::OpServerImpl {
 
             LOG(DEBUG, "message ==" << reply->element[2]->str);
 
-            rapidjson::Document document;	// Default template parameter uses UTF8 and MemoryPoolAllocator.
+            RAPIDJSON_NAMESPACE::Document document;	// Default template parameter uses UTF8 and MemoryPoolAllocator.
             if (document.ParseInsitu<0>(reply->element[2]->str).HasParseError()) {
                 assert(0);
             }
@@ -717,25 +717,25 @@ OpServerProxy::UVENotif(const std::string &type,
     if (deleted) {
         impl_->KafkaPub(pt, kstr.c_str(), genstr, string());
     } else {
-        rapidjson::Document dd;
+        RAPIDJSON_NAMESPACE::Document dd;
         dd.SetObject();
         for (map<string,string>::const_iterator it = value.begin();
                     it != value.end(); it++) {
-            rapidjson::Value sval(rapidjson::kStringType);
+            RAPIDJSON_NAMESPACE::Value sval(RAPIDJSON_NAMESPACE::kStringType);
             sval.SetString((it->second).c_str(), dd.GetAllocator());
-            rapidjson::Value skey(rapidjson::kStringType);
+            RAPIDJSON_NAMESPACE::Value skey(RAPIDJSON_NAMESPACE::kStringType);
             dd.AddMember(skey.SetString(it->first.c_str(), dd.GetAllocator()),
                          sval, dd.GetAllocator());
         }
-        rapidjson::StringBuffer sb;
-        rapidjson::Writer<rapidjson::StringBuffer> writer(sb);
+        RAPIDJSON_NAMESPACE::StringBuffer sb;
+        RAPIDJSON_NAMESPACE::Writer<RAPIDJSON_NAMESPACE::StringBuffer> writer(sb);
         dd.Accept(writer);
         string jsonline(sb.GetString());
 
         impl_->KafkaPub(pt, kstr.c_str(), genstr, jsonline);
     }
 
-    rapidjson::Document dd;
+    RAPIDJSON_NAMESPACE::Document dd;
     dd.SetObject();
 
     return true;

src/analytics/structured_syslog_config.cc
@@ -73,14 +73,14 @@ StructuredSyslogConfig::PollApplicationRecords() {
 }
 
 void
-StructuredSyslogConfig::HostnameRecordsHandler(rapidjson::Document &jdoc,
+StructuredSyslogConfig::HostnameRecordsHandler(RAPIDJSON_NAMESPACE::Document &jdoc,
             boost::system::error_code &ec,
             const std::string &version, int status, const std::string &reason,
             std::map<std::string, std::string> *headers) {
     if (jdoc.IsObject() && jdoc.HasMember("structured-syslog-hostname-records")) {
-        for (rapidjson::SizeType j = 0;
+        for (RAPIDJSON_NAMESPACE::SizeType j = 0;
                     j < jdoc["structured-syslog-hostname-records"].Size(); j++) {
-                const rapidjson::Value& hr = jdoc["structured-syslog-hostname-records"][j];
+                const RAPIDJSON_NAMESPACE::Value& hr = jdoc["structured-syslog-hostname-records"][j];
                 std::string name, hostaddr, tenant, location, device;
 
                 name = hr["display_name"].GetString();
@@ -116,14 +116,14 @@ StructuredSyslogConfig::HostnameRecordsHandler(rapidjson::Document &jdoc,
 }
 
 void
-StructuredSyslogConfig::ApplicationRecordsHandler(rapidjson::Document &jdoc,
+StructuredSyslogConfig::ApplicationRecordsHandler(RAPIDJSON_NAMESPACE::Document &jdoc,
             boost::system::error_code &ec,
             const std::string &version, int status, const std::string &reason,
             std::map<std::string, std::string> *headers) {
     if (jdoc.IsObject() && jdoc.HasMember("structured-syslog-application-records")) {
-        for (rapidjson::SizeType j = 0;
+        for (RAPIDJSON_NAMESPACE::SizeType j = 0;
                     j < jdoc["structured-syslog-application-records"].Size(); j++) {
-                const rapidjson::Value& ar = jdoc["structured-syslog-application-records"][j];
+                const RAPIDJSON_NAMESPACE::Value& ar = jdoc["structured-syslog-application-records"][j];
                 std::string name, app_category, app_subcategory,
                             app_groups, app_risk, app_service_tags;
 
@@ -144,7 +144,7 @@ StructuredSyslogConfig::ApplicationRecordsHandler(rapidjson::Document &jdoc,
                     app_service_tags = ar["structured_syslog_app_service_tags"].GetString();
                 }
 
-                const rapidjson::Value& fq_name = ar["fq_name"];
+                const RAPIDJSON_NAMESPACE::Value& fq_name = ar["fq_name"];
                 std::string tenant_name = fq_name[1].GetString();
                 if (tenant_name.compare("default-global-analytics-config") == 0) {
                     LOG(DEBUG, "Adding ApplicationRecord: " << name);

src/analytics/structured_syslog_config.h
@@ -192,11 +192,11 @@ class StructuredSyslogConfig {
         boost::shared_ptr<TenantApplicationRecord> GetTenantApplicationRecord(const std::string &name);
 
     private:
-        void HostnameRecordsHandler(rapidjson::Document &jdoc,
+        void HostnameRecordsHandler(RAPIDJSON_NAMESPACE::Document &jdoc,
                     boost::system::error_code &ec,
                     const std::string &version, int status, const std::string &reason,
                     std::map<std::string, std::string> *headers);
-        void ApplicationRecordsHandler(rapidjson::Document &jdoc,
+        void ApplicationRecordsHandler(RAPIDJSON_NAMESPACE::Document &jdoc,
                     boost::system::error_code &ec,
                     const std::string &version, int status, const std::string &reason,
                     std::map<std::string, std::string> *headers);

src/analytics/usrdef_counters.cc
@@ -48,13 +48,13 @@ UserDefinedCounters::ReadConfig()
 }
 
 void
-UserDefinedCounters::UDCHandler(rapidjson::Document &jdoc,
+UserDefinedCounters::UDCHandler(RAPIDJSON_NAMESPACE::Document &jdoc,
             boost::system::error_code &ec,
             std::string version, int status, std::string reason,
             std::map<std::string, std::string> *headers)
 {
     if (jdoc.IsObject() && jdoc.HasMember("global-system-configs")) {
-        for (rapidjson::SizeType j=0;
+        for (RAPIDJSON_NAMESPACE::SizeType j=0;
                     j < jdoc["global-system-configs"].Size(); j++) {
 
             if (!jdoc["global-system-configs"][j].HasMember(
@@ -72,13 +72,13 @@ UserDefinedCounters::UDCHandler(rapidjson::Document &jdoc,
                 continue;
             }
 
-            const rapidjson::Value& gsc = jdoc["global-system-configs"][j]
+            const RAPIDJSON_NAMESPACE::Value& gsc = jdoc["global-system-configs"][j]
                     ["user_defined_log_statistics"]["statlist"];
             if (!gsc.IsArray()) {
                 continue;
             }
 
-            for (rapidjson::SizeType i = 0; i < gsc.Size(); i++) {
+            for (RAPIDJSON_NAMESPACE::SizeType i = 0; i < gsc.Size(); i++) {
                 if (!gsc[i].IsObject() || !gsc[i].HasMember("name") ||
                         !gsc[i].HasMember("pattern")) {
                     continue;

src/analytics/usrdef_counters.h
@@ -59,7 +59,7 @@ class UserDefinedCounters {
 
     private:
         void ReadConfig();
-        void UDCHandler(rapidjson::Document &jdoc,
+        void UDCHandler(RAPIDJSON_NAMESPACE::Document &jdoc,
                     boost::system::error_code &ec,
                     std::string version, int status, std::string reason,
                     std::map<std::string, std::string> *headers);

Juniper/contrail-analytics

contrail-collector/OpServerProxy.cc
@@ -429,7 +429,7 @@ class OpServerProxy::OpServerImpl {
 
             LOG(DEBUG, "message ==" << reply->element[2]->str);
 
-            rapidjson::Document document;	// Default template parameter uses UTF8 and MemoryPoolAllocator.
+            contrail_rapidjson::Document document;	// Default template parameter uses UTF8 and MemoryPoolAllocator.
             if (document.ParseInsitu<0>(reply->element[2]->str).HasParseError()) {
                 assert(0);
             }
@@ -717,25 +717,25 @@ OpServerProxy::UVENotif(const std::string &type,
     if (deleted) {
         impl_->KafkaPub(pt, kstr.c_str(), genstr, string());
     } else {
-        rapidjson::Document dd;
+        contrail_rapidjson::Document dd;
         dd.SetObject();
         for (map<string,string>::const_iterator it = value.begin();
                     it != value.end(); it++) {
-            rapidjson::Value sval(rapidjson::kStringType);
+            contrail_rapidjson::Value sval(contrail_rapidjson::kStringType);
             sval.SetString((it->second).c_str(), dd.GetAllocator());
-            rapidjson::Value skey(rapidjson::kStringType);
+            contrail_rapidjson::Value skey(contrail_rapidjson::kStringType);
             dd.AddMember(skey.SetString(it->first.c_str(), dd.GetAllocator()),
                          sval, dd.GetAllocator());
         }
-        rapidjson::StringBuffer sb;
-        rapidjson::Writer<rapidjson::StringBuffer> writer(sb);
+        contrail_rapidjson::StringBuffer sb;
+        contrail_rapidjson::Writer<contrail_rapidjson::StringBuffer> writer(sb);
         dd.Accept(writer);
         string jsonline(sb.GetString());
 
         impl_->KafkaPub(pt, kstr.c_str(), genstr, jsonline);
     }
 
-    rapidjson::Document dd;
+    contrail_rapidjson::Document dd;
     dd.SetObject();
 
     return true;

contrail-collector/structured_syslog_config.cc
@@ -73,14 +73,14 @@ StructuredSyslogConfig::PollApplicationRecords() {
 }
 
 void
-StructuredSyslogConfig::HostnameRecordsHandler(rapidjson::Document &jdoc,
+StructuredSyslogConfig::HostnameRecordsHandler(contrail_rapidjson::Document &jdoc,
             boost::system::error_code &ec,
             const std::string &version, int status, const std::string &reason,
             std::map<std::string, std::string> *headers) {
     if (jdoc.IsObject() && jdoc.HasMember("structured-syslog-hostname-records")) {
-        for (rapidjson::SizeType j = 0;
+        for (contrail_rapidjson::SizeType j = 0;
                     j < jdoc["structured-syslog-hostname-records"].Size(); j++) {
-                const rapidjson::Value& hr = jdoc["structured-syslog-hostname-records"][j];
+                const contrail_rapidjson::Value& hr = jdoc["structured-syslog-hostname-records"][j];
                 std::string name, hostaddr, tenant, location, device;
 
                 name = hr["display_name"].GetString();
@@ -116,14 +116,14 @@ StructuredSyslogConfig::HostnameRecordsHandler(rapidjson::Document &jdoc,
 }
 
 void
-StructuredSyslogConfig::ApplicationRecordsHandler(rapidjson::Document &jdoc,
+StructuredSyslogConfig::ApplicationRecordsHandler(contrail_rapidjson::Document &jdoc,
             boost::system::error_code &ec,
             const std::string &version, int status, const std::string &reason,
             std::map<std::string, std::string> *headers) {
     if (jdoc.IsObject() && jdoc.HasMember("structured-syslog-application-records")) {
-        for (rapidjson::SizeType j = 0;
+        for (contrail_rapidjson::SizeType j = 0;
                     j < jdoc["structured-syslog-application-records"].Size(); j++) {
-                const rapidjson::Value& ar = jdoc["structured-syslog-application-records"][j];
+                const contrail_rapidjson::Value& ar = jdoc["structured-syslog-application-records"][j];
                 std::string name, app_category, app_subcategory,
                             app_groups, app_risk, app_service_tags;
 
@@ -144,7 +144,7 @@ StructuredSyslogConfig::ApplicationRecordsHandler(rapidjson::Document &jdoc,
                     app_service_tags = ar["structured_syslog_app_service_tags"].GetString();
                 }
 
-                const rapidjson::Value& fq_name = ar["fq_name"];
+                const contrail_rapidjson::Value& fq_name = ar["fq_name"];
                 std::string tenant_name = fq_name[1].GetString();
                 if (tenant_name.compare("default-global-analytics-config") == 0) {
                     LOG(DEBUG, "Adding ApplicationRecord: " << name);

contrail-collector/structured_syslog_config.h
@@ -192,11 +192,11 @@ class StructuredSyslogConfig {
         boost::shared_ptr<TenantApplicationRecord> GetTenantApplicationRecord(const std::string &name);
 
     private:
-        void HostnameRecordsHandler(rapidjson::Document &jdoc,
+        void HostnameRecordsHandler(contrail_rapidjson::Document &jdoc,
                     boost::system::error_code &ec,
                     const std::string &version, int status, const std::string &reason,
                     std::map<std::string, std::string> *headers);
-        void ApplicationRecordsHandler(rapidjson::Document &jdoc,
+        void ApplicationRecordsHandler(contrail_rapidjson::Document &jdoc,
                     boost::system::error_code &ec,
                     const std::string &version, int status, const std::string &reason,
                     std::map<std::string, std::string> *headers);

contrail-collector/usrdef_counters.cc
@@ -48,13 +48,13 @@ UserDefinedCounters::ReadConfig()
 }
 
 void
-UserDefinedCounters::UDCHandler(rapidjson::Document &jdoc,
+UserDefinedCounters::UDCHandler(contrail_rapidjson::Document &jdoc,
             boost::system::error_code &ec,
             std::string version, int status, std::string reason,
             std::map<std::string, std::string> *headers)
 {
     if (jdoc.IsObject() && jdoc.HasMember("global-system-configs")) {
-        for (rapidjson::SizeType j=0;
+        for (contrail_rapidjson::SizeType j=0;
                     j < jdoc["global-system-configs"].Size(); j++) {
 
             if (!jdoc["global-system-configs"][j].HasMember(
@@ -72,13 +72,13 @@ UserDefinedCounters::UDCHandler(rapidjson::Document &jdoc,
                 continue;
             }
 
-            const rapidjson::Value& gsc = jdoc["global-system-configs"][j]
+            const contrail_rapidjson::Value& gsc = jdoc["global-system-configs"][j]
                     ["user_defined_log_statistics"]["statlist"];
             if (!gsc.IsArray()) {
                 continue;
             }
 
-            for (rapidjson::SizeType i = 0; i < gsc.Size(); i++) {
+            for (contrail_rapidjson::SizeType i = 0; i < gsc.Size(); i++) {
                 if (!gsc[i].IsObject() || !gsc[i].HasMember("name") ||
                         !gsc[i].HasMember("pattern")) {
                     continue;

contrail-collector/usrdef_counters.h
@@ -59,7 +59,7 @@ class UserDefinedCounters {
 
     private:
         void ReadConfig();
-        void UDCHandler(rapidjson::Document &jdoc,
+        void UDCHandler(contrail_rapidjson::Document &jdoc,
                     boost::system::error_code &ec,
                     std::string version, int status, std::string reason,
                     std::map<std::string, std::string> *headers);