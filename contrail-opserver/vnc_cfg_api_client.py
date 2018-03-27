b97c28712bbc022651413f4a72defc1a2562552a
Jackjvs <vsjagan@juniper.net>
2018-03-27 11:49:29 -0700

    Add RBAC mappings for uve_types that have defined config mappings Get uuid from fq_name and based on permisions process get/post of a specific UVE Partial-Bug: #1750655
    Change-Id: I5a8094091a3feec84e18fbd0162e556fb442b2ff

--- contrail-opserver/vnc_cfg_api_client.py
+++ contrail-opserver/vnc_cfg_api_client.py
@@ -13,6 +13,28 @@ from functools import wraps
 
 class VncCfgApiClient(object):
 
+    config_obj_to_fq_name_prefix_map = {
+        'alarm': 'default-global-system-config',
+        'analytics_node': 'default-global-system-config',
+        'api_access_list': 'default-global-system-config',
+        'application_policy_set': 'default-policy-management',
+        'bgp_router': 'default-domain:default-project:ip-fabric:__default__',
+        'config_node': 'default-global-system-config',
+        'database_node': 'default-global-system-config',
+        'discovery_service_assignment': 'default-discovery-service-assignment',
+        'firewall_policy': 'default-policy-management',
+        'global_qos_config': 'default-global-system-config',
+        'global_system_config': 'default-global-system-config',
+        'global_vrouter_config': 'default-global-system-config',
+        'policy_management': 'default-policy-management',
+        'route_target': 'target',
+        'service_appliance_set': 'default-global-system-config',
+        'service_templete': 'default-domain',
+        'tag_type': '',
+        'virtual_machine_interfcae': 'default-global-system-config',
+        'virtual_router': 'default-global-system-config'
+    }
+
     def __init__(self, conf_info, sandesh_instance, logger):
         self._conf_info = conf_info
         self._sandesh_instance = sandesh_instance
@@ -78,6 +100,23 @@ class VncCfgApiClient(object):
                 self._update_connection_state(ConnectionStatus.DOWN, str(e))
                 time.sleep(3)
     # end connect
+
+    def get_obj_perms_from_name(self, name, cfg_type, token):
+        try:
+            uuid = None
+            rv_obj_perms = None
+            if name.count(":") == 0:
+                fq_name = [self.config_obj_to_fq_name_prefix_map[cfg_type], name]
+            else:
+                fq_name = name.split(":")
+            uuid = self._vnc_api_client.fq_name_to_id(cfg_type, fq_name)
+            self._logger.info("name:%s fq_name:%s uuid:%s" % (name, fq_name, uuid))
+        except Exception as e:
+            self._logger.error("fq_name_to_id: Exception: %s", str(e))
+        else:
+            rv_obj_perms = self._vnc_api_client.obj_perms(token, uuid)
+        return rv_obj_perms
+    # end get_obj_perms_from_name
 
     def get_resource_list(self, obj_type, token):
         try:
