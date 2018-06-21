5fa624beadd1d5fe719c5186e61a0574e896bd16
Jackjvs <vsjagan@juniper.net>
2018-06-20 20:29:53 -0700

    Pass the request scoped keystone token to obj_get_method
    Test access with proj1-user1-token on proj1-net1(data access ok)
    Test access with proj2-user1-token on proj1-net1(data access denied)
    Test access with proj3-user1-token on proj1-net1(data access denied)
    (role of all the above users is _member_)
    
    Change-Id: I4eef5a333784672bf6c0c84134db8f3167b403ce
    Closes-Bug: #1777946

--- contrail-opserver/vnc_cfg_api_client.py
+++ contrail-opserver/vnc_cfg_api_client.py
@@ -117,11 +117,19 @@ class VncCfgApiClient(object):
         else:
             obj_get_method = cfg_type+"_read"
             try:
+                if 'X-USER-TOKEN' in self._vnc_api_client._headers:
+                    self._logger.info("Replace token: %s for get_method:%s uuid:%s", %  \
+                        (self._vnc_api_client._headers['X-USER-TOKEN'], \
+                        obj_get_method, uuid))
+                self._vnc_api_client._headers['X-USER-TOKEN'] = token
                 obj = getattr(self._vnc_api_client, obj_get_method) (id = uuid)
                 rv_obj_perms = {'permissions':'R'}
             except Exception as e:
                 self._logger.error("fq_name:%s cfg_type:%s uuid:%s Exception: %s", \
                     (fq_name, cfg_type, uuid, str(e)) )
+            finally:
+                if 'X-USER-TOKEN' in self._vnc_api_client._headers:
+                    del self._vnc_api_client._headers['X-USER-TOKEN']
         return rv_obj_perms
     # end get_obj_perms_from_name
 
