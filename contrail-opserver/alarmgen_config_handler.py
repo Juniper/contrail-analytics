95c2808568958b46cbd184e7e965e4b601e0481a
Alexey Osorgin <alexey.osorgin@gmail.com>
2019-02-04 13:25:40 +0300

    Support Intent API etcd in alarm-gen service
    Change-Id: I5218b5ecea27a5f09b0991e9ae8f93478f5f76fa
    closes-jira-bug: JBE-891

--- contrail-opserver/alarmgen_config_handler.py
+++ contrail-opserver/alarmgen_config_handler.py
@@ -33,12 +33,12 @@ class AlarmGenConfigHandler(ConfigHandler):
     }
 
     def __init__(self, sandesh, module_id, instance_id, rabbitmq_cfg,
-                 cassandra_cfg, alarm_plugins, alarm_config_change_callback,
-                 host_ip):
+                 cassandra_cfg, etcd_cfg, alarm_plugins,
+                 alarm_config_change_callback, host_ip):
         service_id = socket.getfqdn()+':'+module_id+':'+instance_id
         super(AlarmGenConfigHandler, self).__init__(sandesh, service_id,
-              rabbitmq_cfg, cassandra_cfg, DBBaseAG, self.REACTION_MAP,
-              host_ip)
+              rabbitmq_cfg, cassandra_cfg, etcd_cfg, DBBaseAG,
+              self.REACTION_MAP, host_ip)
         self._alarm_plugins = alarm_plugins
         self._alarm_config_change_callback = alarm_config_change_callback
         self._inbuilt_alarms = {}
--- contrail-opserver/config_handler.py
+++ contrail-opserver/config_handler.py
@@ -2,25 +2,30 @@
 # Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
 #
 
+from gevent import monkey
+monkey.patch_all()
 
 import gevent
+
 import json
 import traceback
 
 from cfgm_common.vnc_amqp import VncAmqpHandle
-from cfgm_common.vnc_object_db import VncObjectDBClient
+from cfgm_common.vnc_etcd import VncEtcdWatchHandle
+from cfgm_common.vnc_object_db import VncObjectDBClient, VncObjectEtcdClient
 from analytics_logger import AnalyticsLogger
 
 
 class ConfigHandler(object):
 
     def __init__(self, sandesh, service_id, rabbitmq_cfg, cassandra_cfg,
-                 db_cls, reaction_map, host_ip):
+                 etcd_cfg, db_cls, reaction_map, host_ip):
         self._sandesh = sandesh
         self._logger = AnalyticsLogger(self._sandesh)
         self._service_id = service_id
         self._rabbitmq_cfg = rabbitmq_cfg
         self._cassandra_cfg = cassandra_cfg
+        self._etcd_cfg = etcd_cfg
         self._db_cls = db_cls
         self._reaction_map = reaction_map
         self._vnc_amqp = None
@@ -31,40 +36,59 @@ class ConfigHandler(object):
     # Public methods
 
     def start(self):
+        #use_etcd = self._etcd_cfg and self._etcd_cfg.host
+        use_etcd = True
         # Connect to rabbitmq for config update notifications
-        rabbitmq_qname = self._service_id
+        self._logger.info("!!!!!!!!!!!!Connecting to ETCD (subscription): %s" % str(self._etcd_cfg))
         while True:
             try:
-                self._vnc_amqp = VncAmqpHandle(self._sandesh, self._logger,
-                    self._db_cls, self._reaction_map, self._service_id,
-                    self._rabbitmq_cfg, self.host_ip)
+                if use_etcd:
+                    self._vnc_amqp = VncAmqpHandle(self._sandesh, self._logger,
+                        self._db_cls, self._reaction_map, self._service_id,
+                        self._rabbitmq_cfg, self.host_ip)
+                else:
+                    self._vnc_amqp = VncEtcdWatchHandle(logger=self._logger,
+                        **self._etcd_cfg)
+                self._logger.info("!!!!!!!!!!Esteblishing connection to ETCD (subscription)")
                 self._vnc_amqp.establish()
             except Exception as e:
-                template = 'Exception {0} connecting to Rabbitmq. Arguments:\n{1!r}'
+                template = '!!!!!!!!!!!!!!Exception {0} connecting to ETCD. Arguments:\n{1!r}'
                 msg = template.format(type(e).__name__, e.args)
                 self._logger.error('%s: %s' % (msg, traceback.format_exc()))
                 gevent.sleep(2)
             else:
                 break
-        cassandra_credential = {
-            'username': self._cassandra_cfg['user'],
-            'password': self._cassandra_cfg['password']
-        }
-        if not all(cassandra_credential.values()):
-            cassandra_credential = None
+
+        self._logger.info("!!!!!!!!!!Connected to ETCD (subscription)")
+
+        # Connect to config Cassandra DB
+        self._logger.info("!!!!!!!!!!!Connecting to ETCD")
         try:
-            self._vnc_db = VncObjectDBClient(self._cassandra_cfg['servers'],
+            if use_etcd:
+                cassandra_credential = {
+                    'username': self._cassandra_cfg['user'],
+                    'password': self._cassandra_cfg['password']
+                }
+                if not all(cassandra_credential.values()):
+                    cassandra_credential = None
+
+                self._vnc_db = VncObjectDBClient(self._cassandra_cfg['servers'],
                 self._cassandra_cfg['cluster_id'], logger=self._logger.log,
                 credential=cassandra_credential,
                 ssl_enabled=self._cassandra_cfg['use_ssl'],
                 ca_certs=self._cassandra_cfg['ca_certs'])
+            else:
+                self._vnc_db = VncObjectEtcdClient(logger=self._logger.log,
+                    **self._etcd_cfg)
         except Exception as e:
             template = 'Exception {0} connecting to Config DB. Arguments:\n{1!r}'
             msg = template.format(type(e).__name__, e.args)
             self._logger.error('%s: %s' % (msg, traceback.format_exc()))
             exit(2)
+        self._logger.info("!!!!!!!!!!!!!!!!!!Connected to ETCD")
         self._db_cls.init(self, self._logger, self._vnc_db)
         self._sync_config_db()
+        self._logger.info("!!!!!!!!!!!!!!!!ETCD is syncronized")
     # end start
 
     def stop(self):
