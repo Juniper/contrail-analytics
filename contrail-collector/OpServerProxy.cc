2f1957463781b89997a1c36683604859509f3d4e
aadarshk <aadarshk@juniper.net>
2019-03-13 18:21:58 +0530

    contrail-analytics-api connection_infos under process_status in the node UVE inconsistent
    Fix on codebase Master
    
    In the case of certain UVEs, the description was missing.
    And Server address was also empty. The above two problems have been fixed.
    
    Change-Id: Ieb2e5cf8e97745432787f08d462c8f53ba6b208c
    closes-jira-bug: JCB-205422

--- contrail-collector/OpServerProxy.cc
+++ contrail-collector/OpServerProxy.cc
@@ -199,7 +199,7 @@ class OpServerProxy::OpServerImpl {
                 }
                 ConnectionState::GetInstance()->Update(ConnectionType::REDIS_UVE,
                 "To", ConnectionStatus::UP, to_ops_conn_->Endpoint(),
-                std::string());
+                "Redis(To) connecting to CallbackProcess");
                 evm_->io_service()->post(boost::bind(&OpServerProxy::OpServerImpl::ToOpsConnUpPostProcess, this));
                 return;
 
@@ -220,7 +220,7 @@ class OpServerProxy::OpServerImpl {
             if (reply.type != REDIS_REPLY_ERROR) {
                 ConnectionState::GetInstance()->Update(ConnectionType::REDIS_UVE,
                 "From", ConnectionStatus::UP, from_ops_conn_->Endpoint(),
-                std::string());
+                "Redis(From) handling the auth callback");
                 evm_->io_service()->post(boost::bind(&OpServerProxy::OpServerImpl::FromOpsConnUpPostProcess, this));
                 return;
 
@@ -298,7 +298,7 @@ class OpServerProxy::OpServerImpl {
             // Update connection info
             ConnectionState::GetInstance()->Update(ConnectionType::REDIS_UVE,
                 "To", ConnectionStatus::DOWN, to_ops_conn_->Endpoint(),
-                std::string());
+                "Redis(To) reconnecting");
             evm_->io_service()->post(boost::bind(&OpServerProxy::OpServerImpl::RAC_ConnectProcess,
                         this, RAC_CONN_TYPE_TO_OPS));
         }
@@ -308,7 +308,7 @@ class OpServerProxy::OpServerImpl {
             // Update connection info
             ConnectionState::GetInstance()->Update(ConnectionType::REDIS_UVE,
                 "From", ConnectionStatus::DOWN, from_ops_conn_->Endpoint(),
-                std::string());
+                "Redis(From) reconnecting");
             evm_->io_service()->post(boost::bind(&OpServerProxy::OpServerImpl::RAC_ConnectProcess,
                         this, RAC_CONN_TYPE_FROM_OPS));
         }
@@ -448,7 +448,7 @@ class OpServerProxy::OpServerImpl {
             // Update connection 
             ConnectionState::GetInstance()->Update(ConnectionType::REDIS_UVE,
                 "To", ConnectionStatus::INIT, to_ops_conn_->Endpoint(),
-                std::string());
+                "Redis(To) connection initializing");
             to_ops_conn_.get()->RAC_Connect();
             from_ops_conn_.reset(new RedisAsyncConnection(evm_,
                 redis_uve_ip, redis_uve_port,
@@ -457,7 +457,7 @@ class OpServerProxy::OpServerImpl {
             // Update connection info
             ConnectionState::GetInstance()->Update(ConnectionType::REDIS_UVE,
                 "From", ConnectionStatus::INIT, from_ops_conn_->Endpoint(),
-                std::string());
+                "Redis(From) connection initializing");
             from_ops_conn_.get()->RAC_Connect();
         }
 
