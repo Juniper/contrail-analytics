2ce2b4b1b87c8520a3806f2f042f1f693a3fd236
Gavril Florian <gflorian@juniper.net>
2021-01-08 14:05:32 +0200

    Cherry pick https://review.opencontrail.org/c/tungstenfabric/tf-analytics/+/56328
    Contrail collector is hogging memory
    
    It was observed that collector is continuosly hogging memory and making
    the system unstable. There was no leak observed when collector was in
    valgrind within UT. Also, core-analyzer utility didn't report any memory
    leak. However it showed that collector is holding ~16 GB of free memory
    and
    it was not released back to system. This was causing memory hog.
    In this fix, malloc_trim call is added to release the memory to system
    periodically.
    This fix is verified on customer setup and since this is called
    periodically,
    any memory hog will be temporary.
    
    closes-jira-bug: CEM-20189
    Change-Id: I783e9e036ef126a2591da05041efcfd2bd5702b8
    (cherry picked from commit 92a2d44bfaa32330a17dc0df535c8e96e8d0f39e)

--- contrail-collector/main.cc
+++ contrail-collector/main.cc
@@ -112,6 +112,12 @@ bool CollectorInfoLogger(VizSandeshContext &ctx) {
 
     analytics->SendGeneratorStatistics();
 
+    /* Releasing process free memory to back to system */
+    int rc = malloc_trim(0);
+    if (!rc) {
+        LOG(ERROR, "Collector free memory is not released to system, rc: " << rc);
+    }
+
     collector_info_log_timer->Cancel();
     collector_info_log_timer->Start(60*1000, boost::bind(&CollectorInfoLogTimer),
                                NULL);
